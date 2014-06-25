610f1ec7028fae7b51913b4b19ca4fc0af666c72
nader-dab <nader.dabour@abv.bg>
2014-06-25 13:47:39 +0300

    Added stylecop integration

--- ExternalLogging/ITraceListenerClient.cs
+++ ExternalLogging/ITraceListenerClient.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ExternalLogging
+﻿namespace ExternalLogging
 {
     /// <summary>
     /// Defines interface for trace listener clients.
